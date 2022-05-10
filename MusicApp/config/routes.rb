a5a2f9c55969bc8bb60c4c0a4f9b4c421b30d2df
Vivian Chen <vnchen92@gmail.com>
2022-05-10 17:33:13 -0400

    Added Albums table and corrected routes

--- MusicApp/config/routes.rb
+++ MusicApp/config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show, :new, :create]
   resource :session, only: [:new, :create, :destroy]
-  resources :bands
+  resources :bands do
+    resources :albums, only: [:new]
+  end
+  resources :albums, only: [:create, :edit, :show, :update, :destroy]
+
 end
--- MusicApp/db/schema.rb
+++ MusicApp/db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_10_162008) do
+ActiveRecord::Schema.define(version: 2022_05_10_210634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "albums", force: :cascade do |t|
+    t.integer "band_id", null: false
+    t.integer "year", null: false
+    t.string "title", null: false
+    t.boolean "live", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["band_id"], name: "index_albums_on_band_id"
+  end
 
   create_table "bands", force: :cascade do |t|
     t.string "name", null: false
